c101f3dc40feffc7f7f2c8503ca32b25741ad191
MateoChacon <xdmateo90@gmail.com>
2023-05-11 17:26:44 -0500

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {},
+      textColor: (theme) => ({
+        ...theme("colors"),
+        primary: "hsl(209, 23%, 22%)",
+
+        secondary: "hsl(207, 26%, 17%)",
+
+        terceary: "hsl(200, 15%, 8%)",
+
+        cuarto: "hsl(0, 0%, 52%)",
+
+        quinto: "hsl(0, 0%, 98%)",
+
+        sexto: "hsl(0, 0%, 100%)",
+      }),
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "hsl(209, 23%, 22%)",
+
+        secondary: "hsl(207, 26%, 17%)",
+
+        terceary: "hsl(200, 15%, 8%)",
+
+        cuarto: "hsl(0, 0%, 52%)",
+
+        quinto: "hsl(0, 0%, 98%)",
+
+        sexto: "hsl(0, 0%, 100%)",
+        septimo: "#f3f4f6",
+      }),
+    },
   },
   plugins: [],
 };
