448e574e294b3fe8dcd4fc319d86699ffa8f24b9
MateoChacon <xdmateo90@gmail.com>
2023-05-11 17:29:48 -0500

    install and add pinia in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+
+const pinia = createPinia()
+const app = createApp(App)
 
 createApp(App).use(router).mount('#app')
