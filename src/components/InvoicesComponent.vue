ae77451c95a5ac3e626d97421a2fa6c13302135d
MateoChacon <xdmateo90@gmail.com>
2023-05-15 21:34:53 -0500

    create component invoices

---
+++ src/components/InvoicesComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="bg-white p-6 rounded-md dark:bg-secondary dark:text-white">
+        <div class="flex justify-between mb-4">
+            <p class=" font-semibold text-lg"><span class="text-blue-300">#</span>RT3080</p>
+            <p class="text-slate-400 font-light dark:text-white">Jensen Huang</p>
+        </div>
+        <div class="flex justify-between items-center">
+            <div>
+                <p class="text-slate-400 font-light dark:text-white">Due 19 Aug 2021</p>
+                <p class="font-bold text-xl">$1,800.90</p>
+            </div>
+            <div class="flex py-4 px-8 bg-orange-100 items-center rounded-lg h-[40px] dark:bg-slate-700">
+                <div class="h-2 w-2 rounded-full bg-green-400 mr-2"></div>
+                <p>Paid</p>
+            </div>
+        </div>
+    </div>
+</template>
