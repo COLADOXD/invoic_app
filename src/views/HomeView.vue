1bd307a7f898eb9d743eef524402863753d08f4f
MateoChacon <xdmateo90@gmail.com>
2023-05-15 21:39:02 -0500

    Create header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import FilterComponent from "../components/FilterComponent.vue"
+import NewComponent from "../components/NewComponent.vue"
+import Invoices from "../components/InvoicesComponent.vue"
 </script>
+
 <template>
-  <div>home view</div>
+  <div class="flex justify-between mb-5 dark:text-white">
+    <div>
+      <p class="text-2xl font-bold">Invoices</p>
+      <p class="text-slate-400">0 voices</p>
+    </div>
+    <div class="flex flex-row">
+      <FilterComponent />
+      <NewComponent />
+    </div>
+  </div>
+  <Invoices />
 </template>
