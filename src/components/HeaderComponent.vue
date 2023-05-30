2bb9578ee76e091cf8c72b1f4c2046211759978d
MateoChacon <xdmateo90@gmail.com>
2023-05-30 10:53:09 -0500

    add router link root

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -19,12 +19,15 @@ const toggleMode = (change: boolean) => {
 </script>
 <template>
     <div class="bg-secondary flex justify-between mb-2 fixed w-full">
-        <div class="p-5 bg-blue-500 rounded-r-2xl">
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="26">
-                <path fill="#FFF" fill-rule="evenodd"
-                    d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z" />
-            </svg>
-        </div>
+        <router-link to="/">
+            <div class="p-5 bg-blue-500 rounded-r-2xl">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="26">
+                    <path fill="#FFF" fill-rule="evenodd"
+                        d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z" />
+                </svg>
+            </div>
+        </router-link>
         <div class="flex mr-5 items-center">
             <div @click="toggleMode(true)">
                 <div v-if="store.isDark">
