92f3918e1d00e558ccd29cfc4e06951d7a87b1de
MateoChacon <xdmateo90@gmail.com>
2023-06-06 21:53:13 -0500

    add function onPaidState

--- src/views/AboutInvoiceView.vue
+++ src/views/AboutInvoiceView.vue
@@ -3,6 +3,7 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 import DetailsInvoiceComponent from "@/components/DetailsInvoiceComponent.vue";
 import GoBackComponent from "@/components/GoBackComponent.vue";
 import editInvoice from "./editInvoice.vue";
+import { Items } from '../stores/state';
 import { useStore } from '@/stores/state';
 import { useRoute } from "vue-router";
 import { useRouter } from 'vue-router';
@@ -13,6 +14,7 @@ const route = useRoute();
 const router = useRouter();
 const param = ref()
 param.value = route.params.index
+const invoice = store.invoices[param.value];
 const viewDeleteInvoice = ref(false)
 
 const deleteInvoice = () => {
@@ -29,6 +31,11 @@ const onEditInvoice = ref(false)
 const viewEditInvoice = (boolean: boolean) => {
   onEditInvoice.value = boolean
 }
+
+const onStatePaid = () => {
+  invoice.stateInvoice = 'Paid'
+}
+
 </script>
 
 <template>
@@ -39,9 +46,8 @@ const viewEditInvoice = (boolean: boolean) => {
       <div v-if="store.invoices.length > 0"
         class="fixed z-20 h-[190px] max-w-[480px] bg-white dark:text-white dark:bg-secondary p-5 w-[87.5%] top-[350px] rounded-lg">
         <p class="font-bold text-2xl">Confirm Delete</p>
-        <!-- {{ param }} -->
         <p class="text-sm text-slate-400">Are you sure you want to delete invoice #{{
-          store.invoices[param].codeInvoice
+          invoice.codeInvoice
         }}? This action
           cannot be undone.</p>
         <div class="flex justify-end mt-3">
@@ -62,7 +68,7 @@ const viewEditInvoice = (boolean: boolean) => {
         <div class="text-slate-400">Status</div>
         <div class="flex py-4 px-8 bg-orange-100 items-center rounded-lg h-[40px] dark:bg-slate-700">
           <div class="h-2 w-2 rounded-full bg-green-400 mr-2"></div>
-          <p>Paid</p>
+          <p>{{ invoice.stateInvoice }}</p>
         </div>
       </div>
       <DetailsInvoiceComponent />
@@ -73,10 +79,11 @@ const viewEditInvoice = (boolean: boolean) => {
         class="bg-slate-200 dark:bg-cuarto p-2 text-xs rounded-3xl font-semibold">Edit</button>
       <button @click="confirmDeleteInvoice"
         class="bg-red-500 p-2 text-xs rounded-3xl mx-3 font-semibold text-white">Delete</button>
-      <div class="bg-blue-500 p-2 text-xs rounded-3xl font-semibold text-white">Mark as Paid</div>
+      <button @click="onStatePaid" class="bg-blue-500 p-2 text-xs rounded-3xl font-semibold text-white">Mark as
+        Paid</button>
     </div>
   </div>
   <div v-else>
-    <editInvoice :invoice="store.invoices[param]" />
+    <editInvoice @view-edit="viewEditInvoice" :invoice="store.invoices[param]" />
   </div>
 </template>
